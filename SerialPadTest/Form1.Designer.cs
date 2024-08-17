55386451e3720888708afa7cf4e40c92756dfe59
hakonoko <hakonoko05@gmail.com>
2024-08-17 16:04:07 +0900

    プロジェクト ファイルを追加します。

---
+++ SerialPadTest/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace SerialPadTest {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.comboBoxSelectPort = new System.Windows.Forms.ComboBox();
+            this.Button_Connect = new System.Windows.Forms.Button();
+            this.Button_UP = new System.Windows.Forms.Button();
+            this.Button_LEFT = new System.Windows.Forms.Button();
+            this.Button_RIGHT = new System.Windows.Forms.Button();
+            this.Button_DOWN = new System.Windows.Forms.Button();
+            this.Button_SELECT = new System.Windows.Forms.Button();
+            this.Button_START = new System.Windows.Forms.Button();
+            this.button_disconnect = new System.Windows.Forms.Button();
+            this.Button_X = new System.Windows.Forms.Button();
+            this.Button_Y = new System.Windows.Forms.Button();
+            this.Button_A = new System.Windows.Forms.Button();
+            this.Button_B = new System.Windows.Forms.Button();
+            this.Button_R = new System.Windows.Forms.Button();
+            this.Button_L = new System.Windows.Forms.Button();
+            this.button_ReloadSeralPort = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // comboBoxSelectPort
+            // 
+            this.comboBoxSelectPort.FormattingEnabled = true;
+            this.comboBoxSelectPort.Location = new System.Drawing.Point(12, 12);
+            this.comboBoxSelectPort.Name = "comboBoxSelectPort";
+            this.comboBoxSelectPort.Size = new System.Drawing.Size(121, 20);
+            this.comboBoxSelectPort.TabIndex = 0;
+            // 
+            // Button_Connect
+            // 
+            this.Button_Connect.Location = new System.Drawing.Point(139, 9);
+            this.Button_Connect.Name = "Button_Connect";
+            this.Button_Connect.Size = new System.Drawing.Size(75, 23);
+            this.Button_Connect.TabIndex = 1;
+            this.Button_Connect.Text = "Connect";
+            this.Button_Connect.UseVisualStyleBackColor = true;
+            this.Button_Connect.Click += new System.EventHandler(this.Button_Connect_Click);
+            // 
+            // Button_UP
+            // 
+            this.Button_UP.Location = new System.Drawing.Point(49, 125);
+            this.Button_UP.Name = "Button_UP";
+            this.Button_UP.Size = new System.Drawing.Size(75, 23);
+            this.Button_UP.TabIndex = 2;
+            this.Button_UP.Text = "UP";
+            this.Button_UP.UseVisualStyleBackColor = true;
+            this.Button_UP.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Button_UP_MouseDown);
+            this.Button_UP.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button_UP_MouseDown);
+            // 
+            // Button_LEFT
+            // 
+            this.Button_LEFT.Location = new System.Drawing.Point(12, 154);
+            this.Button_LEFT.Name = "Button_LEFT";
+            this.Button_LEFT.Size = new System.Drawing.Size(75, 23);
+            this.Button_LEFT.TabIndex = 3;
+            this.Button_LEFT.Text = "LEFT";
+            this.Button_LEFT.UseVisualStyleBackColor = true;
+            this.Button_LEFT.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Button_LEFT_MouseDown);
+            this.Button_LEFT.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button_LEFT_MouseDown);
+            // 
+            // Button_RIGHT
+            // 
+            this.Button_RIGHT.Location = new System.Drawing.Point(93, 154);
+            this.Button_RIGHT.Name = "Button_RIGHT";
+            this.Button_RIGHT.Size = new System.Drawing.Size(75, 23);
+            this.Button_RIGHT.TabIndex = 4;
+            this.Button_RIGHT.Text = "RIGHT";
+            this.Button_RIGHT.UseVisualStyleBackColor = true;
+            this.Button_RIGHT.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Button_RIGHT_MouseDown);
+            this.Button_RIGHT.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button_RIGHT_MouseDown);
+            // 
+            // Button_DOWN
+            // 
+            this.Button_DOWN.Location = new System.Drawing.Point(49, 183);
+            this.Button_DOWN.Name = "Button_DOWN";
+            this.Button_DOWN.Size = new System.Drawing.Size(75, 23);
+            this.Button_DOWN.TabIndex = 5;
+            this.Button_DOWN.Text = "DOWN";
+            this.Button_DOWN.UseVisualStyleBackColor = true;
+            this.Button_DOWN.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Button_DOWN_MouseDown);
+            this.Button_DOWN.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button_DOWN_MouseDown);
+            // 
+            // Button_SELECT
+            // 
+            this.Button_SELECT.Location = new System.Drawing.Point(212, 154);
+            this.Button_SELECT.Name = "Button_SELECT";
+            this.Button_SELECT.Size = new System.Drawing.Size(75, 23);
+            this.Button_SELECT.TabIndex = 6;
+            this.Button_SELECT.Text = "SELECT";
+            this.Button_SELECT.UseVisualStyleBackColor = true;
+            this.Button_SELECT.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Button_SELECT_Click);
+            this.Button_SELECT.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button_SELECT_Click);
+            // 
+            // Button_START
+            // 
+            this.Button_START.Location = new System.Drawing.Point(293, 154);
+            this.Button_START.Name = "Button_START";
+            this.Button_START.Size = new System.Drawing.Size(75, 23);
+            this.Button_START.TabIndex = 7;
+            this.Button_START.Text = "START";
+            this.Button_START.UseVisualStyleBackColor = true;
+            this.Button_START.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Button_START_Click);
+            this.Button_START.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button_START_Click);
+            // 
+            // button_disconnect
+            // 
+            this.button_disconnect.Location = new System.Drawing.Point(220, 9);
+            this.button_disconnect.Name = "button_disconnect";
+            this.button_disconnect.Size = new System.Drawing.Size(75, 23);
+            this.button_disconnect.TabIndex = 8;
+            this.button_disconnect.Text = "Disconnect";
+            this.button_disconnect.UseVisualStyleBackColor = true;
+            this.button_disconnect.Click += new System.EventHandler(this.Button_Disconnect_Click);
+            // 
+            // Button_X
+            // 
+            this.Button_X.Location = new System.Drawing.Point(455, 125);
+            this.Button_X.Name = "Button_X";
+            this.Button_X.Size = new System.Drawing.Size(75, 23);
+            this.Button_X.TabIndex = 9;
+            this.Button_X.Text = "X";
+            this.Button_X.UseVisualStyleBackColor = true;
+            this.Button_X.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Button_X_Click);
+            this.Button_X.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button_X_Click);
+            // 
+            // Button_Y
+            // 
+            this.Button_Y.Location = new System.Drawing.Point(414, 154);
+            this.Button_Y.Name = "Button_Y";
+            this.Button_Y.Size = new System.Drawing.Size(75, 23);
+            this.Button_Y.TabIndex = 10;
+            this.Button_Y.Text = "Y";
+            this.Button_Y.UseVisualStyleBackColor = true;
+            this.Button_Y.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Button_Y_Click);
+            this.Button_Y.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button_Y_Click);
+            // 
+            // Button_A
+            // 
+            this.Button_A.Location = new System.Drawing.Point(495, 154);
+            this.Button_A.Name = "Button_A";
+            this.Button_A.Size = new System.Drawing.Size(75, 23);
+            this.Button_A.TabIndex = 11;
+            this.Button_A.Text = "A";
+            this.Button_A.UseVisualStyleBackColor = true;
+            this.Button_A.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Button_A_Click);
+            this.Button_A.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button_A_Click);
+            // 
+            // Button_B
+            // 
+            this.Button_B.Location = new System.Drawing.Point(455, 183);
+            this.Button_B.Name = "Button_B";
+            this.Button_B.Size = new System.Drawing.Size(75, 23);
+            this.Button_B.TabIndex = 12;
+            this.Button_B.Text = "B";
+            this.Button_B.UseVisualStyleBackColor = true;
+            this.Button_B.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Button_B_Click);
+            this.Button_B.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button_B_Click);
+            // 
+            // Button_R
+            // 
+            this.Button_R.Location = new System.Drawing.Point(355, 96);
+            this.Button_R.Name = "Button_R";
+            this.Button_R.Size = new System.Drawing.Size(75, 23);
+            this.Button_R.TabIndex = 13;
+            this.Button_R.Text = "R";
+            this.Button_R.UseVisualStyleBackColor = true;
+            this.Button_R.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Button_R_Click);
+            this.Button_R.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button_R_Click);
+            // 
+            // Button_L
+            // 
+            this.Button_L.Location = new System.Drawing.Point(153, 96);
+            this.Button_L.Name = "Button_L";
+            this.Button_L.Size = new System.Drawing.Size(75, 23);
+            this.Button_L.TabIndex = 14;
+            this.Button_L.Text = "L";
+            this.Button_L.UseVisualStyleBackColor = true;
+            this.Button_L.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Button_L_Click);
+            this.Button_L.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button_L_Click);
+            // 
+            // button_ReloadSeralPort
+            // 
+            this.button_ReloadSeralPort.Location = new System.Drawing.Point(355, 9);
+            this.button_ReloadSeralPort.Name = "button_ReloadSeralPort";
+            this.button_ReloadSeralPort.Size = new System.Drawing.Size(75, 23);
+            this.button_ReloadSeralPort.TabIndex = 15;
+            this.button_ReloadSeralPort.Text = "Reload";
+            this.button_ReloadSeralPort.UseVisualStyleBackColor = true;
+            this.button_ReloadSeralPort.Click += new System.EventHandler(this.button_ReloadSeralPort_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 236);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(558, 202);
+            this.richTextBox1.TabIndex = 16;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button_ReloadSeralPort);
+            this.Controls.Add(this.Button_L);
+            this.Controls.Add(this.Button_R);
+            this.Controls.Add(this.Button_B);
+            this.Controls.Add(this.Button_A);
+            this.Controls.Add(this.Button_Y);
+            this.Controls.Add(this.Button_X);
+            this.Controls.Add(this.button_disconnect);
+            this.Controls.Add(this.Button_START);
+            this.Controls.Add(this.Button_SELECT);
+            this.Controls.Add(this.Button_DOWN);
+            this.Controls.Add(this.Button_RIGHT);
+            this.Controls.Add(this.Button_LEFT);
+            this.Controls.Add(this.Button_UP);
+            this.Controls.Add(this.Button_Connect);
+            this.Controls.Add(this.comboBoxSelectPort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxSelectPort;
+        private System.Windows.Forms.Button Button_Connect;
+        private System.Windows.Forms.Button Button_UP;
+        private System.Windows.Forms.Button Button_LEFT;
+        private System.Windows.Forms.Button Button_RIGHT;
+        private System.Windows.Forms.Button Button_DOWN;
+        private System.Windows.Forms.Button Button_SELECT;
+        private System.Windows.Forms.Button Button_START;
+        private System.Windows.Forms.Button button_disconnect;
+        private System.Windows.Forms.Button Button_X;
+        private System.Windows.Forms.Button Button_Y;
+        private System.Windows.Forms.Button Button_A;
+        private System.Windows.Forms.Button Button_B;
+        private System.Windows.Forms.Button Button_R;
+        private System.Windows.Forms.Button Button_L;
+        private System.Windows.Forms.Button button_ReloadSeralPort;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ SerialPadTest/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerialPadTest {
+
+    public partial class Form1 : Form {
+        // Clock Cycle: 1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16
+        // Button     : B  Y  Se St U  D  L  R  A  X  L  R  0  0  0  0 
+        // Bit        : 15 14 13 12 11 10 9  8  7  6  5  4  3  2  1  0
+
+        internal enum Button {
+            B = 15,
+            Y = 14,
+            Select = 13,
+            Start = 12,
+            Up = 11,
+            Down = 10,
+            Left = 9,
+            Right = 8,
+            A = 7,
+            X = 6,
+            L = 5,
+            R = 4,
+        }
+
+        internal readonly string[] buttonNames = { "B", "Y", "SELECT", "START", "UP", "DOWN", "LEFT", "RIGHT", "A", "X", "L", "R", "0", "0", "0", "0" };
+
+        SerialPort serialPort = new SerialPort();
+
+        ushort buttons = 0;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            LoadSerialPortList();
+        }
+
+        private void LoadSerialPortList() {
+            comboBoxSelectPort.Items.Clear();
+
+            string[] ports = SerialPort.GetPortNames();
+            foreach (var port in ports) {
+                comboBoxSelectPort.Items.Add(port);
+            }
+
+            if (comboBoxSelectPort.Items.Count > 0) {
+                comboBoxSelectPort.SelectedIndex = 0;
+            }
+        }
+
+        private void Button_Connect_Click(object sender, EventArgs e) {
+            object selectedObject = comboBoxSelectPort.SelectedItem;
+
+            if (serialPort.IsOpen || comboBoxSelectPort.SelectedIndex == -1) {
+                return;
+            }
+
+            try {
+                serialPort.BaudRate = 115200;
+                serialPort.Parity = Parity.None;
+                serialPort.DataBits = 8;
+                serialPort.StopBits = StopBits.One;
+                serialPort.Handshake = Handshake.None;
+                serialPort.PortName = comboBoxSelectPort.GetItemText(selectedObject);
+                serialPort.WriteBufferSize = 2;
+                serialPort.DataReceived += SerialPort_DataReceived;
+                serialPort.Open();
+
+                //DebugWriteLine($"Serial \"{serialPort.PortName}\" Connected.");
+                buttons = 0;
+
+                DebugWriteLine($"Serial \"{serialPort.PortName}\" connect Success!");
+
+            } catch {
+                DebugWriteLine($"Serial \"{serialPort.PortName}\" connect failed.");
+            }
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e) {
+            var str = serialPort.ReadExisting();
+            if (!string.IsNullOrEmpty(str)) {
+                StringBuilder sb = new StringBuilder();
+                sb.Append(str);
+                if (InvokeRequired) {
+                    this.Invoke(new Action<string>(this.DebugWriteLine), sb.ToString());
+
+                }
+            }
+
+        }
+
+        private void Button_Disconnect_Click(object sender, EventArgs e) {
+            if (serialPort.IsOpen) {
+                serialPort.Close();
+                DebugWriteLine($"Serial \"{serialPort.PortName}\" Disconnected.");
+            }
+        }
+
+        private void button_ReloadSeralPort_Click(object sender, EventArgs e) {
+            LoadSerialPortList();
+        }
+
+        private void SetButtonToggle(Button button) {
+            int bit = (int)button;
+
+            int data = (~buttons >> bit) & 1;
+            ushort mask = (ushort)~(0x0000 | (1 << bit));
+
+            buttons &= mask;
+            buttons |= (ushort)(data << bit);
+
+            SerialWriteButtons(buttons);
+        }
+
+        //private void SetButton(int bit, bool isPush) {
+        //    if (isPush) {
+        //        buttons |= (ushort)(1 << bit);
+        //    } else {
+        //        ushort mask = (ushort)~(0x0000 | (1 << bit));
+        //        buttons &= mask;
+        //    }
+
+        //    SerialWriteButtons(buttons);
+        //}
+        
+        private void SerialWriteButtons(ushort buttons) {
+            byte high = (byte)((buttons & 0xFF00) >> 8);
+            byte low = (byte)(buttons & 0x00FF);
+
+            if(serialPort.IsOpen)
+                serialPort.Write(new byte[] { high, low }, 0, 2);
+                                            
+            DebugWriteLine("Data: " + (high).ToString("X2") + (low).ToString("X2"));
+            if (buttons > 0) {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("Button: ");
+                for (int i = 15; i >= 0; i--) {
+                    if (((buttons >> i) & 0x0001) == 1) {
+                        sb.Append(buttonNames[15 - i] + ", ");
+                    }
+                }
+                sb.Remove(sb.Length - 2, 1);
+
+                DebugWriteLine(sb.ToString());
+            } else {
+                DebugWriteLine("Button: None.");
+            }
+        }
+
+        private void DebugWriteLine(string str) {
+            if (!string.IsNullOrEmpty(str)) {
+                richTextBox1.AppendText(str);
+                richTextBox1.AppendText(Environment.NewLine);
+                richTextBox1.ScrollToCaret();
+                Debug.WriteLine(str);
+            }
+        }
+
+        private void Button_UP_MouseDown(object sender, MouseEventArgs e) {
+            SetButtonToggle(Button.Up);
+        }
+
+        private void Button_LEFT_MouseDown(object sender, MouseEventArgs e) {
+            SetButtonToggle(Button.Left);
+        }
+
+        private void Button_RIGHT_MouseDown(object sender, MouseEventArgs e) {
+            SetButtonToggle(Button.Right);
+        }
+
+        private void Button_DOWN_MouseDown(object sender, MouseEventArgs e) {
+            SetButtonToggle(Button.Down);
+        }
+
+        private void Button_L_Click(object sender, MouseEventArgs e) {
+            SetButtonToggle(Button.L);
+        }
+
+        private void Button_R_Click(object sender, MouseEventArgs e) {
+            SetButtonToggle(Button.R);
+        }
+
+        private void Button_SELECT_Click(object sender, MouseEventArgs e) {
+            SetButtonToggle(Button.Select);
+        }
+
+        private void Button_START_Click(object sender, MouseEventArgs e) {
+            SetButtonToggle(Button.Start);
+        }
+
+        private void Button_X_Click(object sender, MouseEventArgs e) {
+            SetButtonToggle(Button.X);
+        }
+
+        private void Button_Y_Click(object sender, MouseEventArgs e) {
+            SetButtonToggle(Button.Y);
+        }
+
+        private void Button_A_Click(object sender, MouseEventArgs e) {
+            SetButtonToggle(Button.A);
+        }
+
+        private void Button_B_Click(object sender, MouseEventArgs e) {
+            SetButtonToggle(Button.B);
+        }
+    }
+}
